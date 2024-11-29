d1a785bb3620010a7b6f717eade4fa67a7a3a49f
Jorge <jorgevelascogamedev@gmail.com>
2024-11-29 13:01:06 +0100

    -Props added

--- Player/flashlight.gd
+++ Player/flashlight.gd
@@ -1,15 +1,28 @@
 extends SpotLight3D
 
+@export var extra_time_on_pickup := 40.0
+
+@onready var tweakling_timer: Timer = $TweaklingTimer
 @onready var flashlight_battery: Timer = $FlashlightBattery
 @onready var starting_battery_time := flashlight_battery.wait_time
+
+var battery_below_25_percent := false
 var empty_battery := false
+
 
 func _ready() -> void:
 	visible = false
 
 
+func _process(delta: float) -> void:
+	if flashlight_battery.time_left < starting_battery_time / 4.0:
+		battery_below_25_percent = true
+	else:
+		battery_below_25_percent = false
+
+
 func toggle_flashlight() -> void:
-	#play sound
+	#play sound 
 	if empty_battery:
 		return
 	
@@ -25,9 +38,10 @@ func toggle_flashlight() -> void:
 
 
 func add_battery() -> void:
+	var remaining_time := flashlight_battery.time_left
 	flashlight_battery.stop()
 	#play sound
-	flashlight_battery.start(starting_battery_time)
+	flashlight_battery.start(remaining_time + extra_time_on_pickup)
 
 
 func _on_flashlight_battery_timeout() -> void:
