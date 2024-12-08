ef6c92fd92d420bdabb1158025bc383ba8efcd20
Jorge <jorgevelascogamedev@gmail.com>
2024-12-08 03:11:16 +0100

    -Added a small animation to withdraw the gun

--- scripts/state_machine/states/aim.gd
+++ scripts/state_machine/states/aim.gd
@@ -22,6 +22,7 @@ func enter(_msg : ={}) -> void:
 	player.velocity = Vector3.ZERO
 	world_camera.shaking = true
 	aiming_gun.visible = true
+	#small animation...
 	var initial_gun_pos := aiming_gun.position
 	aiming_gun.global_position.y -= .4
 	var tween = create_tween()
@@ -61,6 +62,12 @@ func handle_camera_rotation(_delta:float) -> void:
 
 func exit() -> void:
 	world_camera.shaking = false
+	var tween = create_tween()
+	var original_pos := aiming_gun.position
+	var new_pos := Vector3(aiming_gun.position.x, aiming_gun.position.y - 0.4, aiming_gun.position.z)
+	tween.tween_property(aiming_gun, "position", new_pos, 0.3)
+	await tween.finished
+	aiming_gun.position = original_pos
 	player.camera_pivot.rotation.z = 0
 	aiming_gun.visible = false
 
