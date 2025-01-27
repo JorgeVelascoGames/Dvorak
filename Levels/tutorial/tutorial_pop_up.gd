3cd9beaa0aebf4d5cd83004e7e3e2164ecb31112
DESKTOP-00AP5J0\jorge <jorgevelascogamedev@gmail.com>
2025-01-27 12:51:45 +0100

    -Working on tutorial -Working on blender environment

--- Levels/tutorial/tutorial_pop_up.gd
+++ Levels/tutorial/tutorial_pop_up.gd
@@ -1,13 +1,27 @@
 extends Control
 class_name TutorialPopUp
 
+@export var open_on_ready := false
+@export var open_on_ready_dealy := 0.0
+##Wait said amount of time once the tutorial has already been triggered to show
+@export var delay_to_show := 0.0
+
+var already_displayed := false
 
 func _ready() -> void:
 	hide()
-	await get_tree().create_timer(4).timeout
-	open_tutorial()
+	
+	if open_on_ready:
+		await get_tree().create_timer(open_on_ready_dealy).timeout
+		open_tutorial()
 
 func open_tutorial() -> void:
+	if already_displayed:
+		return
+	
+	already_displayed = true
+	
+	await get_tree().create_timer(delay_to_show).timeout
 	show()
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 	get_tree().paused = true
@@ -21,3 +35,8 @@ func close_tutorial() -> void:
 
 func _on_continue_button_pressed() -> void:
 	close_tutorial()
+
+
+func _on_player_detector_body_entered(body: Node3D) -> void:
+	if body is Player:
+		open_tutorial()
--- dope/Dvorak/Arte/Estandarización.md
+++ dope/Dvorak/Arte/Estandarización.md
@@ -3,5 +3,6 @@
 
 -Para los zocalos del suelo: El extrude con Alt+S a o.02. Luego bajamos el loop del borde otros 0.02
 -Para hacer la división superior de la pared, como es un tercio de la pared (menos el zócalo), lo que hacemos es hacer un loop cut con dos cortes, y disolvemos el inferior. Así nos queda el cacho de pared de azulejos que es 2/3 de la pared, desde el zócalo hasta el suelo. Para darle algo de volumen, le hacemos un simple extrude con Alt+S de 0.01
+-Para los zocalos entre pared baja y alta, hacemos 12 loopcuts, y disolvemos los inferiores. Luego hacemos Alt + S a 0.02
 
 -Pantalla: 320x240 (revisar)
